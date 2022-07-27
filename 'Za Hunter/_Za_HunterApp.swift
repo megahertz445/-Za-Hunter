5b763ddad1f03cb3f4236df1bb3ded14f1597f90
Ela Murat <ela.murat@icloud.com>
2022-07-27 10:52:39 -0500

    Initial Commit

---
+++ 'Za Hunter/_Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_HunterApp.swift
+//  'Za Hunter
+//
+//  Created by Ela Murat on 27/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct _Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
