3f1df4bd09e7357a39810570d105a602f4b45b62
Ela Murat <ela.murat@icloud.com>
2022-07-27 13:15:17 -0500

    Added address info + links to website and directions

--- 'Za Hunter/LocationDetailsView.swift
+++ 'Za Hunter/LocationDetailsView.swift
@@ -11,9 +11,29 @@ import MapKit
 struct LocationDetailsView: View {
     var selectedMapItem: MKMapItem
     var body: some View {
+        let address = selectedMapItem.placemark.subThoroughfare! + "" + //street address number
+        selectedMapItem.placemark.thoroughfare! + "\n" + //street name
+        selectedMapItem.placemark.locality! + ", " + //state
+        selectedMapItem.placemark.postalCode! //zip code
         VStack {
+            Text(address)
             Text(selectedMapItem.name!)
                 .font(.title)
+                .padding()
+            Link(destination: selectedMapItem.url!) {
+                Text("Webstie")
+                    .padding()
+            }
+            Button {
+                let latitude = selectedMapItem.placemark.coordinate.latitude
+                let longitude = selectedMapItem.placemark.coordinate.longitude
+                let url = URL(string: "maps://?daddr=\(latitude),\(longitude)")
+                if UIApplication.shared.canOpenURL(url!) {
+                    UIApplication.shared.open(url!, options: [:])
+                }
+            } label: {
+                Text("Directions")
+            }
         }
     }
 }
